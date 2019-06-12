a347506eecdbb53a7ea62abd7f2346c7fba3ffe1
Dario Susic <dario.susic@gmail.com>
2019-06-12 07:25:54 +0200

    added login

--- rest/Config.class.php
+++ rest/Config.class.php
@@ -3,10 +3,15 @@
 class Config
 {
     const DB = [
+    	'host' => 'ibu-db-server.adnan.dev:3306',
+        'username' => 'sara-r',
+        'password' => 'aeVrjn8L',
+        'scheme' => 'db_sara_r'
+    	/*
         'host' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'scheme' => 'expense_tracking_system'
+        'scheme' => 'expense_tracking_system'*/
     ];
 
     const JWT_SECRET = 'secret';
